710932c2cbdf0c64dc54c720ae9acb25d1231ba7
Valera97531 <10002000valera@gmail.com>
2022-01-25 21:21:39 +0200

    begin project Burger, header and main-screen

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,16 @@
 import * as flsFunctions from "./modules/function.js";
 flsFunctions.isWebp();
+
+document.addEventListener("DOMContentLoaded", () => {
+	let headerSearch = document.querySelector('.header__search'),
+		headerSearching = document.querySelector('.header__searching'),
+		header = document.querySelector('.header');
+
+	headerSearch.addEventListener('click', () => {
+		headerSearching.classList.toggle('_active');
+		header.classList.toggle('header__active');
+	})
+
+});
+
+
