ab32af9a44cde0107f0bb31de4348e166dad1fe6
Valera97531 <10002000valera@gmail.com>
2022-01-29 15:46:23 +0200

    begin animate

--- src/js/app.js
+++ src/js/app.js
@@ -7,14 +7,19 @@ flsFunctions.isWebp();
 document.addEventListener("DOMContentLoaded", () => {
 	let headerSearch = document.querySelector('.header__search'),
 		headerSearching = document.querySelector('.header__searching'),
-		header = document.querySelector('.header');
+		header = document.querySelector('.header'),
+		close = document.querySelector('#close'),
+		search = document.querySelector('svg.search');
 
 	if (headerSearching) {
 		headerSearch.addEventListener('click', () => {
 			headerSearching.classList.toggle('_active');
 			header.classList.toggle('header__active');
+			close.classList.toggle('vis');
+			search.classList.toggle('none');
 		})
 	};
+
 
 
 });
